59635a4b45881f75b61e92238587486d330ecdd1
Telecaster525 <good.day.just.chill@gmail.com>
2024-04-29 23:00:01 +0300

    feat: added shadows and themes

--- script.js
+++ script.js
@@ -4,3 +4,42 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+const themeToggleBtn = document.getElementById('theme-toggle-btn');
+const socialsContainer = document.getElementById('socials-container');
+const themeToggleHamburgerBtn = document.getElementById('theme-toggle-hamburger-btn');
+
+themeToggleBtn.addEventListener('click', toggleDarkTheme);
+
+function toggleDarkTheme() {
+    const darkStyle = document.getElementById('dark-style');
+    const isDarkThemeEnabled = darkStyle.disabled;
+
+    if (isDarkThemeEnabled) {
+        darkStyle.disabled = false;
+        updateSocialsIcons('github-dark.png', 'telegram-dark.png');
+        updateThemeToggleIcons('theme-dark.png');
+    } else {
+        darkStyle.disabled = true;
+        updateSocialsIcons('github.png', 'telegram.png');
+        updateThemeToggleIcons('theme-light.png');
+    }
+}
+
+function updateSocialsIcons(githubIcon, telegramIcon) {
+    const githubIconElement = socialsContainer.querySelector('[alt="My Github profile"]');
+    const telegramIconElement = socialsContainer.querySelector('[alt="My Telegram profile"]');
+
+    if (githubIconElement) {
+        githubIconElement.src = `./assets/${githubIcon}`;
+    }
+
+    if (telegramIconElement) {
+        telegramIconElement.src = `./assets/${telegramIcon}`;
+    }
+}
+
+function updateThemeToggleIcons(themeIcon) {
+    themeToggleBtn.innerHTML = `<img src="./assets/${themeIcon}" alt="Toggle Dark Theme">`;
+    themeToggleHamburgerBtn.innerHTML = `<img class="theme-toggle-btn" src="./assets/${themeIcon}" alt="Toggle Light Theme">`;
+}
